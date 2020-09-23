6a239fc4b0baca00e0f4d44a0c4d06579004fd48
Arpit Chauhan <arpitc980@gmail.com>
2020-09-23 13:13:45 +0530

    Matrix Multiplication

---
+++ Matrix1/Matrix1/src/Matrix1.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arpit
+ */
+import java.util.Scanner;
+
+public class Matrix1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of rows of first matrix: ");
+        int rowsMatrix1 = scanner.nextInt();
+        System.out.println("Enter number of columns in first matrix/rows in second matrix: ");
+        int columnsMatrix1RowsMatrix2 = scanner.nextInt();
+        System.out.print("Enter number of columns in second matrix : ");
+        int columnsMatrix2 = scanner.nextInt();
+        int[][] matrix1 = new int[rowsMatrix1][columnsMatrix1RowsMatrix2];
+        int[][] matrix2 = new int[columnsMatrix1RowsMatrix2][columnsMatrix2];
+        System.out.println("Enter the first matrix in elements :");
+        for (int i = 0; i < matrix1.length; i++)
+        {
+            for (int j = 0; j < matrix1[0].length; j++)
+            {
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Enter the second matrix in elements :");
+        for (int i = 0; i < matrix2.length; i++)
+        {
+            for (int j = 0; j < matrix2[0].length; j++)
+            {
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+        int[][] productMatrix  = new int[rowsMatrix1][columnsMatrix2];
+        for (int i = 0; i < rowsMatrix1; i++)
+        {
+            for (int j = 0; j < columnsMatrix2; j++) 
+            {
+                for (int k = 0; k < columnsMatrix1RowsMatrix2; k++)
+                {
+                    productMatrix[i][j] = productMatrix[i][j] + matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        System.out.println("\nFirst matrix is : ");
+        for (int i = 0; i < rowsMatrix1; i++)
+        {
+            for (int j = 0; j < columnsMatrix1RowsMatrix2; j++)
+            {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nSecond matrix is : ");
+        for (int i = 0; i < columnsMatrix1RowsMatrix2; i++)
+        {
+            for (int j = 0; j < columnsMatrix2; j++)
+            {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nProduct of matrix1 and matrix2 is");
+        for (int i = 0; i < rowsMatrix1; i++)
+        {
+            for (int j = 0; j < columnsMatrix2; j++)
+            {
+                System.out.print(productMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
